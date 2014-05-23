cd5a8be1a9b3c796953a274a34b70ef2cd3a4096
James Kirk <james.kirk84@gmail.com>
2014-05-23 13:06:23 +0100

    Initial commit

---
+++ ParenthesisBalancing/Balancer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParenthesisBalancing
+{
+    public class Balancer
+    {
+        public static bool IsBalanced(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParenthesisBalancing/Tests.cs
@@ -0,0 +1,38 @@
+﻿
+using NUnit.Framework;
+
+namespace ParenthesisBalancing
+{
+    [TestFixture]
+    public class Tests
+    {
+        /*
+         * Remember the rules:
+         *      No for loops
+         *      No LINQ
+         *      All variables must be declared within the scope of IsBalanced, (i.e no static variables or class members)
+         */
+
+        [Test]
+        public void CorrectlyPairedNestedShouldReturnTrue()
+        {
+            Assert.True(Balancer.IsBalanced("(if (zero? x) max (/ 1 x))"));
+        }
+     
+        [Test]
+        public void CorrectlyPairedSequenceShouldReturnTrue()
+        {
+            Assert.True(Balancer.IsBalanced("I told him (that it’s not yet done). (But he wasn’t listening)"));
+        }
+        [Test]
+        public void OneSingleParenthesisShouldReturnFalse()
+        {
+            Assert.False(Balancer.IsBalanced(":-)"));
+        }
+        [Test]
+        public void SimplyCountingParenthesisIsNotEnough()
+        {
+            Assert.False(Balancer.IsBalanced("())("));
+        }
+    }
+}
